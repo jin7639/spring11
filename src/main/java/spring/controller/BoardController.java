8482aa7f806153f39c5611295d6915f6e67ddd54
Jinny <98166529+jin7639@users.noreply.github.com>
2022-06-22 03:30:43 +0900

    insert

--- src/main/java/spring/controller/BoardController.java
+++ src/main/java/spring/controller/BoardController.java
@@ -2,32 +2,35 @@ package spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import spring.dao.BaordDao;
 import spring.dto.BoardDto;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.SQLException;
-
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    private HttpServletRequest request;
 
-    @GetMapping("/write")
-    public String write(){
+
+    //4. 게시물 쓰기 페이지
+    @GetMapping("/save")
+    public String save() {
+
         return "board/write";
     }
-    @PostMapping("/write")
-    @ResponseBody
-    public boolean write(BoardDto boardDto) throws SQLException, ClassNotFoundException {
-        boolean result = BaordDao.save(boardDto);
-        return result;
-    }
-    @GetMapping("/update")
-    public String update(){
-        return "board/update";
+    /////////////////////////////////////////2. service 매핑 ///////////////////////////////////////////////
+    //1. C
+    @PostMapping("/save")
+    @ResponseBody   // 템플릿 아닌 객체 반환
+    public String save(BoardDto boardDto ) throws SQLException, ClassNotFoundException {
+        System.out.println("save");
+        BaordDao.save(boardDto);
+        return "board/write";
     }
 }
